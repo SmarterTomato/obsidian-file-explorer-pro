7dc55f92f07ba7c85479726685938884f63b4ddd
Tommy Zhao <tommywkzhao@hotmail.com>
2022-07-30 15:36:14 +1000

    Update first header
    ## New
    ### Update first header
    - Add command to update first file header according to file name. Default hotkey `Alt + H`
    - Add function to automatically run above command on file rename
    - Allow user to remove timestamp at the beginning of the file name
    
    ### Updated
    - `Reveal Active File` button in view action should hide when no file is opened
    - Updated some setting description
    
    ## Bug fixes
    - `Reveal Active File` button in view action sometimes not shown properly

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { Plugin } from 'obsidian';
 import { DeleteSelectedFilesManager } from './features/delete-selected-files/delete-selected-files.manager';
 import { RevealActiveFileManager } from './features/reveal-active-file/reveal-active-file.manager';
+import { UpdateFirstHeaderManager } from './features/update-first-header/update-first-header.manager';
 import { Settings } from './interfaces/settings';
 import { HtmlSelector } from './obsidian/obsidian-constants';
 import { SettingTab } from './settings/setting-tab';
@@ -10,13 +11,17 @@ const DEFAULT_SETTINGS: Settings = {
 	showFileExplorerRevealButton: true,
 	showViewActionsRevealButton: true,
 	enableDeleteSelectedFiles: true,
+	addUpdateFirstHeaderCommand: true,
+	autoUpdateFirstHeader: true,
+	ignoreTimestamp: true,
 };
 
-export default class FileExplorerProPlugin extends Plugin {
+export class FileExplorerProPlugin extends Plugin {
 	settings: Settings;
 
 	revealActiveFileManager: RevealActiveFileManager;
 	deleteSelectedFilesManager: DeleteSelectedFilesManager;
+	updateFirstHeaderManager: UpdateFirstHeaderManager;
 
 	async onload() {
 		await this.loadSettings();
@@ -24,12 +29,21 @@ export default class FileExplorerProPlugin extends Plugin {
 		// - Add reveal file button to file explorer
 		this.revealActiveFileManager = new RevealActiveFileManager(this);
 		this.app.workspace.onLayoutReady(() => {
-			this.revealActiveFileManager.init(this.settings);
+			this.revealActiveFileManager.showFileExplorerRevealButton(this.settings.showFileExplorerRevealButton);
+		});
+
+		// - Show reveal file button to view actions only if some file is opened
+		this.app.workspace.on('layout-change', () => {
+			this.revealActiveFileManager.showViewActionsRevealButton(this.settings.showViewActionsRevealButton);
 		});
 
 		// - Add delete selected items command
 		this.deleteSelectedFilesManager = new DeleteSelectedFilesManager(this);
 		this.deleteSelectedFilesManager.init(this.settings);
+
+		// - Auto rename file header
+		this.updateFirstHeaderManager = new UpdateFirstHeaderManager(this);
+		this.updateFirstHeaderManager.init(this.settings);
 
 		// - Add obsidian setting page
 		this.addSettingTab(new SettingTab(this.app, this));
@@ -55,6 +69,10 @@ export default class FileExplorerProPlugin extends Plugin {
 		return this.getMarkdown()?.view.containerEl.querySelector(HtmlSelector.ViewActionsContainer);
 	}
 
+	getActiveFile() {
+		return this.app.workspace.getActiveFile();
+	}
+
 	async loadSettings() {
 		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
 	}
@@ -64,4 +82,4 @@ export default class FileExplorerProPlugin extends Plugin {
 	}
 }
 
-
+export default FileExplorerProPlugin;
--- src/settings/setting-tab.ts
+++ src/settings/setting-tab.ts
@@ -1,5 +1,5 @@
 import { App, PluginSettingTab, Setting } from 'obsidian';
-import FileExplorerProPlugin from '../main';
+import { FileExplorerProPlugin } from '../main';
 
 export class SettingTab extends PluginSettingTab {
   plugin: FileExplorerProPlugin;
@@ -13,8 +13,9 @@ export class SettingTab extends PluginSettingTab {
     const { containerEl } = this;
 
     containerEl.empty();
+    containerEl.createEl('h2', { text: 'File Explorer Pro Settings' });
 
-    containerEl.createEl('h2', { text: 'General Settings' });
+    containerEl.createEl('h3', { text: 'Reveal Active File' });
 
     new Setting(containerEl)
       .setName('Show Reveal Active File Button in File Explorer')
@@ -22,7 +23,7 @@ export class SettingTab extends PluginSettingTab {
         .setValue(this.plugin.settings.showFileExplorerRevealButton)
         .onChange(async (value) => {
           this.plugin.settings.showFileExplorerRevealButton = value;
-          this.plugin.revealActiveFileManager.init(this.plugin.settings);
+          this.plugin.revealActiveFileManager.showFileExplorerRevealButton(value);
           await this.plugin.saveSettings();
         }));
 
@@ -32,16 +33,60 @@ export class SettingTab extends PluginSettingTab {
         .setValue(this.plugin.settings.showFileExplorerRevealButton)
         .onChange(async (value) => {
           this.plugin.settings.showViewActionsRevealButton = value;
-          this.plugin.revealActiveFileManager.init(this.plugin.settings);
+          this.plugin.revealActiveFileManager.showViewActionsRevealButton(value);
           await this.plugin.saveSettings();
         }));
 
+    containerEl.createEl('h3', { text: 'Delete Selected Files' });
+
     new Setting(containerEl)
-      .setName('Enable Alt+Delete to Delete Selected Files in File Explorer')
+      .setName('Enable Delete Selected Files')
+      .setDesc(
+        'Requires restart obsidian. ' +
+        'Add a command to delete selected files in file explorer. ' +
+        'Default hotkey is "Alt+Delete". ')
       .addToggle(toggle => toggle
         .setValue(this.plugin.settings.enableDeleteSelectedFiles)
         .onChange(async (value) => {
           this.plugin.settings.enableDeleteSelectedFiles = value;
+          await this.plugin.saveSettings();
+        }));
+
+    containerEl.createEl('h3', { text: 'Auto File Header' });
+
+    new Setting(containerEl)
+      .setName('Add Update First File Header Command')
+      .setDesc(
+        'Requires restart obsidian. ' +
+        'Add a command to automatically update first header to the current file name. ')
+      .addToggle(toggle => toggle
+        .setValue(this.plugin.settings.addUpdateFirstHeaderCommand)
+        .onChange(async (value) => {
+          this.plugin.settings.addUpdateFirstHeaderCommand = value;
+          await this.plugin.saveSettings();
+        }));
+
+    new Setting(containerEl)
+      .setName('Auto Update First File Header')
+      .setDesc(
+        'Requires restart obsidian. ' +
+        'Automatically run the command to update first header when file rename. ')
+      .addToggle(toggle => toggle
+        .setValue(this.plugin.settings.autoUpdateFirstHeader)
+        .onChange(async (value) => {
+          this.plugin.settings.autoUpdateFirstHeader = value;
+          await this.plugin.saveSettings();
+        }));
+
+    new Setting(containerEl)
+      .setName('Ignore Timestamp in File Name')
+      .setDesc(
+        'First header will ignore the timestamp in the file name. ' +
+        'E.g. Remove random number or timestamp at the beginning of the file name')
+      .addToggle(toggle => toggle
+        .setValue(this.plugin.settings.ignoreTimestamp)
+        .onChange(async (value) => {
+          this.plugin.settings.ignoreTimestamp = value;
           await this.plugin.saveSettings();
         }));
   }
